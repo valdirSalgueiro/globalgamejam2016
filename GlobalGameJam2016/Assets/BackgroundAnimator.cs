c1cbc63fc28756ea61d06f670ccf5373b05e3239
valdirSalgueiro <sombraextra@gmail.com>
2016-01-30 19:38:10 -0200

    Background Estrelas

---
+++ GlobalGameJam2016/Assets/BackgroundAnimator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundAnimator : MonoBehaviour {
+
+    SpriteRenderer renderer;
+    float opacity = 0f;
+    float sign = 1;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+        opacity = Random.Range(0f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.color = new Color(1f, 1f, 1f, opacity);
+
+        if (opacity >= 1.0f)
+        {
+            sign = -1;
+        }
+        if (opacity <= 0) {
+            sign = 1;
+        }
+        opacity += Time.deltaTime * sign;
+	}
+}
